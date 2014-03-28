80560fcbc31a2892af07a76bc5c4016bb4fb32dc
Pedro Axelrud <pedroaxl@gmail.com>
2014-03-28 12:05:21 -0300

    dnsmasq required in metadata of maileed

--- maileed/metadata.rb
+++ maileed/metadata.rb
@@ -6,6 +6,7 @@ description      'Installs/Configures maileed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
+depends 'git'
 depends 'python', '~> 1.4.6'
 depends 'redisio', '~> 1.7.1'
 depends 'bluepill', '~> 2.3.1'
