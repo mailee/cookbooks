8f37455d1b7e3ae11287108220c8a91242de74a4
Pedro Axelrud <pedroaxl@gmail.com>
2014-05-12 16:23:06 -0300

    mailee-links
    a cookbook with mailee nginx redirect confs

---
+++ mailee-links/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: mailee-links
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+config_file = "#{node[:nginx][:dir]}/sites-available/#{node[:deploy][:mailee_staging][:application]}"
+
+if File.exists? config_file
+
+  template config_file do
+    source "nginx-site.erb"
+    owner "root"
+    group "root"
+    mode 0644
+  end
+
+  service 'nginx' do
+    action [:reload]
+    provider Chef::Provider::Service::Upstart
+  end
+  
+end
