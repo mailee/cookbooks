6042ee0be595c8d477e4462b8b4c5d9260baad53
Pedro Axelrud <pedroaxl@gmail.com>
2014-05-13 15:17:20 -0300

    new approach to mailee-links

--- mailee-links/recipes/default.rb
+++ mailee-links/recipes/default.rb
@@ -7,21 +7,53 @@
 # All rights reserved - Do Not Redistribute
 #
 
-config_file = "#{node[:nginx][:dir]}/sites-available/#{node[:deploy][:mailee_staging][:application]}"
+template = """
+  # Mailee.me specific redirects
+  # even with the paths below changed on maileed, we need this to
+  # take care of the clients with custom urls
 
-if File.exists? config_file
-  name = node["opsworks"]["applications"].first["slug_name"]
-  @application = node["deploy"][name]
-  template config_file do
-    source "nginx-site.erb"
+  # empty image for delivery count
+  location /go/view/ {
+    rewrite      ^ http://event-api.mailee.me$request_uri? permanent;
+  }
+
+  # external link
+  location /go/click {
+    rewrite      ^ http://event-api.mailee.me$request_uri? permanent;
+  }
+
+  # this is to keep the assets of old messages working
+  location ~* /(clients|contacts|galleries|header_images|messages|templates|wireframes)/\d+/\w+/\w+/.*\.(gif|jpg|jpeg|png|bmp|tiff|svg)$ {
+    rewrite      ^ http://assets.mailee.me$request_uri?;
+  }
+
+  location ~* /clients/\w+/\w+/.*\.(gif|jpg|jpeg|png|bmp|tiff|svg)$ {
+    rewrite      ^ http://tassets.mailee.me$request_uri?;
+  }
+
+  location ~* /clients/\w+/\w+/\w+/.*\.(gif|jpg|jpeg|png|bmp|tiff|svg)$ {
+    rewrite      ^ http://tassets.mailee.me$request_uri?;
+  }
+  location ~* /clients/\w+/\w+/\w+/\w+/.*\.(gif|jpg|jpeg|png|bmp|tiff|svg)$ {
+    rewrite      ^ http://tassets.mailee.me$request_uri?;
+  }
+
+  error_page 500 502 503 504 /500.html;
+"""
+
+filename = Dir.entries("/etc/nginx/sites-available").find {|f| f.match(/mailee/)}
+
+f = File.read("/etc/nginx/sites-available/#{filename}")
+unless f.match("assets.mailee.me")
+  file "/etc/nginx/sites-available/#{filename}" do
     owner "root"
     group "root"
-    mode 0644
+    mode "0644"
+    action :create
+    content f.gsub(/error_page (.*);$/, template)
   end
-
   service 'nginx' do
     action [:reload]
     provider Chef::Provider::Service::Upstart
   end
-
 end
