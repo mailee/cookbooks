6ecfa6a6f3d8f1c0c393675d0f585085566f9d40
Pablo Lerina <pabloalr@gmail.com>
2014-07-01 18:34:14 -0300

    fix monitrc permission

--- opsworks_agent_monit/recipes/unicorn.rb
+++ opsworks_agent_monit/recipes/unicorn.rb
@@ -5,7 +5,7 @@ include_recipe "opsworks_agent_monit::service"
 node[:deploy].each do |application, deploy|
   template File.join(node[:monit][:conf_dir], "#{application}_unicorn.monitrc") do
     source "unicorn.monitrc.erb"
-    mode 0644
+    mode 0640
     variables(:deploy => deploy, :application => application)
     notifies :restart, resources(:service => "monit")
   end
