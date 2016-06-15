d0115ba7dbf936c3f5761f5de15b6bea4c31a4fd
Pablo Lerina <pabloalr@gmail.com>
2016-06-15 17:50:25 -0300

    comenting

--- maileed/metadata.rb
+++ maileed/metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures maileed'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-depends 'git'
+#depends 'git'
 depends 'python', '~> 1.4.6'
 depends 'redisio', '~> 1.7.1'
 depends 'dnsmasq', '~> 0.2.0'
